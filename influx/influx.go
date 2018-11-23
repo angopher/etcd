51e3707a6b274af4e65dc0150124f094ead25141
liliedison <liliedison@didichuxing.com>
2018-11-23 22:21:50 +0800

    support http transport

--- influx/influx.go
+++ influx/influx.go
@@ -1,7 +1,12 @@
 package influx
 
 import (
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"context"
     "go.etcd.io/etcd/raft"
+    "go.etcd.io/etcd/raft/raftpb"
 )
 
 func main() {
@@ -23,8 +28,36 @@ func main() {
     }
 
     srv.StartNode(peers)
-    srv.Run()
-}
+    go srv.Run()
 
+    http.HandleFunc("/val", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(srv.val)
+	})
+    http.HandleFunc("/set_val", func(w http.ResponseWriter, r *http.Request) {
+		val := r.FormValue("val")
+		fmt.Println("recv val:", val)
+		srv.Propose(srv.Node, context.Background(), []byte(val))
+	})
+    http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		defer w.Write([]byte("ok"))
+
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		var msg raftpb.Message
+		err = msg.Unmarshal(data)
+		if err != nil {
+			panic(err)
+		}
+		srv.recvRaftRPC(srv.Node, context.Background(), msg)
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", 8888), nil)
+    if err != nil {
+        fmt.Printf("msg=ListenAndServe failed,err=%s\n", err.Error())
+    }
+}
 
 
--- influx/server.go
+++ influx/server.go
@@ -2,11 +2,17 @@ package influx
 
 import (
     "context"
+	"net/http"
+	"net"
+	"bytes"
+	"time"
+	"fmt"
     "go.etcd.io/etcd/raft"
     "go.etcd.io/etcd/raft/raftpb"
 )
 
 type RaftServer struct {
+	val string
     Node raft.Node
     raftConfig *raft.Config
     Storage raft.Storage
@@ -45,12 +51,24 @@ func (s *RaftServer) saveToStorage(h raftpb.HardState, es []raftpb.Entry, sn raf
 }
 
 func (s *RaftServer) send(messages []raftpb.Message) {
+	for _, msg := range messages {
+		data, err := msg.Marshal()
+		if err != nil {
+			panic(err)
+		}
+		err = Request("http://127.0.0.1:8888/message", data)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func (s *RaftServer) processSnapshot(sn raftpb.Snapshot) {
 }
 
 func (s *RaftServer) process(entry raftpb.Entry) {
+	fmt.Println("process set val:", string(entry.Data))
+	s.val = string(entry.Data)
 }
 
 func (s *RaftServer) recvRaftRPC(n raft.Node, ctx context.Context, m raftpb.Message) error {
@@ -64,4 +82,37 @@ func (s *RaftServer) Propose(n raft.Node, ctx context.Context, data[]byte) error
 func (s *RaftServer) ProposeConfChange(n raft.Node, ctx context.Context, cc raftpb.ConfChange) error {
     return n.ProposeConfChange(ctx, cc)
 }
+
+
+func Request(url string, data []byte) error {
+	req, err := http.NewRequest("POST", url, bytes.NewReader(data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Connection", "close")
+
+	client := http.Client{
+		Transport: &http.Transport{
+			Dial: func(netw, addr string) (net.Conn, error) {
+				deadline := time.Now().Add(10 * time.Second) //TODO: timeout from config
+				c, err := net.DialTimeout(netw, addr, time.Second)
+				if err != nil {
+					return nil, err
+				}
+				c.SetDeadline(deadline)
+				return c, nil
+			},
+		},
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	return nil
+}
+
 
