ba07b9989ef8cf71aa9a14523c533f4805ce9446
liliedison <liliedison@didichuxing.com>
2018-11-24 12:21:53 +0800

    ...

--- influx/node/influx.go
+++ influx/node/influx.go
@@ -1,29 +1,39 @@
-package influx
+package main
 
 import (
 	"net/http"
 	"io/ioutil"
 	"fmt"
+	"flag"
 	"context"
     "go.etcd.io/etcd/raft"
+    "go.etcd.io/etcd/influx"
     "go.etcd.io/etcd/raft/raftpb"
 )
 
 func main() {
+	selfID := flag.Int("id", 0, "self id")
+	id2 := flag.Int("id2", 0, "peer node")
+	id3 := flag.Int("id3", 0, "peer node")
+	flag.Parse()
+
+	fmt.Println(*selfID, *id2, *id3)
+
     storage := raft.NewMemoryStorage()
     c := &raft.Config{
-        ID:              0x01,
+        ID:              uint64(*selfID),
         ElectionTick:    10,
         HeartbeatTick:   1,
         Storage:         storage,
         MaxSizePerMsg:   4096,
         MaxInflightMsgs: 256,
     }
-    peers := []raft.Peer{{ID: 0x02}, {ID: 0x03}}
+    peers := []raft.Peer{{ID: uint64(*id2)}, {ID: uint64(*id3)}}
 
-    srv := &RaftServer{
-        raftConfig: c,
+    srv := &influx.RaftServer{
+        RaftConfig: c,
         Storage: storage,
+		PeersAddr: make(map[uint64]string),
         Done: make(chan struct{}),
     }
 
@@ -31,14 +41,17 @@ func main() {
     go srv.Run()
 
     http.HandleFunc("/val", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(srv.val)
+		fmt.Println(srv.Val)
 	})
     http.HandleFunc("/set_val", func(w http.ResponseWriter, r *http.Request) {
 		val := r.FormValue("val")
 		fmt.Println("recv val:", val)
 		srv.Propose(srv.Node, context.Background(), []byte(val))
+		fmt.Println("propose done")
+		w.Write([]byte("ok"))
 	})
     http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("recv message")
 		defer w.Write([]byte("ok"))
 
 		data, err := ioutil.ReadAll(r.Body)
@@ -51,10 +64,11 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		srv.recvRaftRPC(srv.Node, context.Background(), msg)
+		srv.RecvRaftRPC(srv.Node, context.Background(), msg)
 	})
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", 8888), nil)
+	fmt.Println("listen:", influx.NodeAddr(uint64(*selfID)))
+	err := http.ListenAndServe(influx.NodeAddr(uint64(*selfID)), nil)
     if err != nil {
         fmt.Printf("msg=ListenAndServe failed,err=%s\n", err.Error())
     }
--- influx/server.go
+++ influx/server.go
@@ -12,20 +12,32 @@ import (
 )
 
 type RaftServer struct {
-	val string
+	Val string
     Node raft.Node
-    raftConfig *raft.Config
-    Storage raft.Storage
+    RaftConfig *raft.Config
+    Storage *raft.MemoryStorage
+	PeersAddr map[uint64]string
     Done chan struct{}
 }
 
+func NodeAddr(id uint64) string {
+	return fmt.Sprintf("127.0.0.1:800%d", id)
+}
+
 func (s *RaftServer) StartNode(peers []raft.Peer) {
-    s.Node = raft.StartNode(s.raftConfig, peers)
+	for _, peer := range peers {
+		s.PeersAddr[peer.ID] = NodeAddr(peer.ID)
+	}
+	fmt.Printf("peers:%+v\n", peers)
+    s.Node = raft.StartNode(s.RaftConfig, peers)
 }
 
 func (s *RaftServer) Run() {
+	t := time.NewTicker(20 * time.Millisecond)
     for {
         select {
+		case <-t.C:
+			s.Node.Tick()
         case rd := <-s.Node.Ready():
             s.saveToStorage(rd.HardState, rd.Entries, rd.Snapshot)
             s.send(rd.Messages)
@@ -34,11 +46,6 @@ func (s *RaftServer) Run() {
             }
             for _, entry := range rd.CommittedEntries {
                 s.process(entry)
-                if entry.Type == raftpb.EntryConfChange {
-                    var cc raftpb.ConfChange
-                    cc.Unmarshal(entry.Data)
-                    s.Node.ApplyConfChange(cc)
-                }
             }
             s.Node.Advance()
         case <-s.Done:
@@ -53,14 +60,14 @@ func (s *RaftServer) saveToStorage(h raftpb.HardState, es []raftpb.Entry, sn raf
 	s.Storage.ApplySnapshot(sn)
 }
 
-//TODO:get peer addr by peer id
 func (s *RaftServer) send(messages []raftpb.Message) {
+	fmt.Println("send msg num:", len(messages))
 	for _, msg := range messages {
 		data, err := msg.Marshal()
 		if err != nil {
 			panic(err)
 		}
-		err = Request("http://127.0.0.1:8888/message", data)
+		err = Request(fmt.Sprintf("%s/message", NodeAddr(msg.To)), data)
 		if err != nil {
 			panic(err)
 		}
@@ -68,14 +75,27 @@ func (s *RaftServer) send(messages []raftpb.Message) {
 }
 
 func (s *RaftServer) processSnapshot(sn raftpb.Snapshot) {
+	fmt.Println("processSnapshot")
 }
 
 func (s *RaftServer) process(entry raftpb.Entry) {
-	fmt.Println("process set val:", string(entry.Data))
-	s.val = string(entry.Data)
+	fmt.Println("process entry")
+
+	if entry.Type == raftpb.EntryConfChange {
+		var cc raftpb.ConfChange
+		cc.Unmarshal(entry.Data)
+		fmt.Printf("conf change: %+v\n", cc)
+		s.Node.ApplyConfChange(cc)
+	} else if len(entry.Data) == 0 {
+		fmt.Println("empty entry")
+	} else {
+		fmt.Println("process set val:", string(entry.Data))
+		s.Val = string(entry.Data)
+	}
+
 }
 
-func (s *RaftServer) recvRaftRPC(n raft.Node, ctx context.Context, m raftpb.Message) error {
+func (s *RaftServer) RecvRaftRPC(n raft.Node, ctx context.Context, m raftpb.Message) error {
     return n.Step(ctx, m)
 }
 
